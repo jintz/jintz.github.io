9ec9ec0e1f7268a1ac3639f14ba243bd9bb33584
Anton Wiegert <anton.wiegert@gmail.com>
2020-09-15 09:19:07 -0400

    [Automation] Update site files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 var typewriter = new Typewriter('#typewriterapp', {
-    strings: ['Pentest Student','CTF Player','Gamer','Game Developer'],
+    strings: ['Pentest Student','CTF Player','Cybersecurity Enthusiast','Ethical Hacker'],
     autoStart: true,
     loop: true,
     cursor: '█',
+    deleteSpeed: 30,
+    delay: 75,
     wrapperClassName: 'typeText',
     cursorClassName: 'typeCursor'
 });
